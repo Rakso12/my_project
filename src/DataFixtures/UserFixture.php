54acc5266c8e4fc9ee61fca61456740a0ae0165c
oki12 <oskartyniec10@wp.pl>
2021-07-07 11:04:05 +0200

    backup #1 - Day 4
    - login with BASIC Auth -> done
    - form to login & welcome user page -> done
    - translation for error "invalid credentials" -> done
    
    todo: api token

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -5,10 +5,18 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixture extends Fixture
 {
-    private static $firstName = [
+    private $passwordHasher;
+
+     public function __construct(UserPasswordHasherInterface $passwordHasher)
+     {
+         $this->passwordHasher = $passwordHasher;
+     }
+
+    private static $firstNameArray = [
         'Jim',
         'Simon',
         'Carl',
@@ -19,12 +27,26 @@ class UserFixture extends Fixture
         'Peter',
     ];
 
+    private static $passwordArray = [
+        'P@ssword',
+        'CommonPassword',
+        'Carl12345',
+        'MyLittleDog',
+        'SuperDangerPassword',
+        'qwerty',
+        'zaqwsx123',
+    ];
+
     public function load(ObjectManager $manager)
     {
         for($i = 1; $i < 5; $i++) {
             $user = new User();
             $user->setEmail('uzytkownik'.$i.'@xyz.com');
-            $user->setFirstName(self::$firstName[$i]);
+            $user->setFirstName(self::$firstNameArray[$i]);
+            $user->setPassword($this->passwordHasher->hashPassword(
+                $user,
+                self::$passwordArray[$i]
+            ));
             $manager->persist($user);
         }
         $manager->flush();
