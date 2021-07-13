d4ad5cd4fa8e57b414f1d15b29825174a77a5803
oki12 <oskartyniec10@wp.pl>
2021-07-13 12:22:48 +0200

    back d8

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,44 +2,72 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class SecurityController extends AbstractController
 {
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     /**
      * @Route("/trylogin", name="loginOAuth", methods={"POST"})
      * @param Request $request
      * @param LoginFormAuthenticator $loginFormAuthenticator
      * @return JsonResponse
      */
-    public function oauthLogin(Request $request, LoginFormAuthenticator $loginFormAuthenticator): JsonResponse
+    public function oauthLogin(Request $request): JsonResponse
     {
-        $data = json_decode($request->getContent(),true);
+        $data = json_decode($request->getContent(), true);
 
-       // $client_id = $data['client_id'];
-       // $client_secret = $data['client_secret'];
-        $username = $data['email'];
+        $username = $data['username'];
         $password = $data['password'];
+        $client_id = $data['client_id'];
+        $client_secret = $data['client_secret'];
 
         $userData[] = [
             'email' => $username,
             'password' => $password,
         ];
 
-        if($loginFormAuthenticator->authenticate($userData))
+        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $username]);
+
+        $token = new UsernamePasswordToken($user, $user->getPassword(), "public", $user->getRoles());
+
+        $this->get("security.token_storage")->setToken($token);
+
+        $event = new InteractiveLoginEvent($request, $token);
+        $this->get("event_dispatcher")->dispatch("security.interactive_login", $event);
+
+        $us = $user->getEmail();
+        $pas = $user->getPassword();
+
+       /* if($us == 'sjohnson@xyz.com') //todo zrobić tu porządne sprawdzanie usera
         {
-            return JsonRespose(['Status' => 'ok ->'.$username], Response::HTTP_OK);
+            $response = $this->client->request('POST',
+                'https://127.0.0.1:8000/token');
         }
-        return JsonResponse(['Status' => 'nope'], Response::HTTP_NO_CONTENT);
+        else
+        {
+            $response = 'NOT GOOD...';
+        }*/
+        /*
+
+        */
+        return new JsonResponse(['Password' => $us], Response::HTTP_OK);
     }
 
     /**
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -22,19 +22,19 @@ class UserRepository extends ServiceEntityRepository
     // /**
     //  * @return User[] Returns an array of User objects
     //  */
-    /*
-    public function findByExampleField($value)
+
+    public function findByEmail($email)
     {
         return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
+            ->andWhere('u.email = :email')
+            ->setParameter('email', $email)
             ->orderBy('u.id', 'ASC')
             ->setMaxResults(10)
             ->getQuery()
             ->getResult()
         ;
     }
-    */
+
 
     /*
     public function findOneBySomeField($value): ?User
